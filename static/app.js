954cd6fbbba33e371aa47195cb831d5d60bc609d
Douglas <dz@bitzend.net>
2021-06-28 16:19:32 -0600

    support nav and content

--- static/app.js
+++ static/app.js
@@ -2,11 +2,36 @@ var app = new Vue({
   el: '#app',
   vuetify: new Vuetify(),
 
-  data:{
-    page: "home"
-  },
-  methods:{
-    
-  }
+  data () {
+   return {
+     dialog: false,
+     nav: [
+       {
+         icon: 'home',
+         text: 'Home',
+         title: 'Back to Home page',
+         active: true
+       },
+       {
+         icon: 'info',
+         text: 'About',
+         title: 'About this demo',
+         active: false
+       },
+       {
+         icon: 'assignment_turned_in',
+         text: 'Todos',
+         title: 'Some stuff that needs doing',
+         active: false
+       },
+       {
+         icon: 'email',
+         text: 'Contact',
+         title: 'Our Contact info',
+         active: false
+       }
+     ]
+   }
+ }
 
 })
